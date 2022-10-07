8a9b2d61946f05b0f3192102ccb2aa03b7fb2574
Polina <polina@blsv.space>
2022-10-07 14:30:56 +0400

    changed the import and the path of the 'main' page due to a new app creation.

--- surveyForm/urls.py
+++ surveyForm/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import main
+from form.views import main
 
 urlpatterns = [
     path('', main),
